4b2a4b6d778fcb16615b74fdc4f295e09b5c9522
Lucas de Ruijter <lucas.deruijter@reyez.nl>
2025-10-06 12:30:22 +0200

    created toggle light theme switch

--- public/views/components/navbar.php
+++ public/views/components/navbar.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title><?= isset($title) ? $title : 'Portfolio' ?></title>
-    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/script.js" defer></script>
 </head>
 <body>
 <h1>Dit is de <?= isset($title) ? strtolower($title) : 'home' ?> page</h1>
@@ -12,5 +13,7 @@
         <a href="./projects">Projects</a> |
         <a href="./contact">Contact</a> |
     </nav>
+    <br>
+    <button onclick="toggleTheme()">Toggle light theme</button>
 </body>
 </html>
