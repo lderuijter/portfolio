e2d9aaa0f1296422328b57c0320a7aa024f49e00
Lucas de Ruijter <lucas.deruijter@reyez.nl>
2025-10-06 17:33:12 +0200

    fixed 404 page

--- public/views/404.php
+++ public/views/404.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>404</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
-<body>
-    <h1>404 page not found</h1>
-    <a href="./home">return to Home page</a>
-    <?php include 'components/footer.php'; ?>
-</body>
-</html>
+<?php $title = '404'; include 'components/navbar.php'; ?>
+<?php include 'components/footer.php'; ?>
+
--- public/views/components/navbar.php
+++ public/views/components/navbar.php
@@ -14,16 +14,28 @@ $theme = isset($_SESSION['theme']) ? $_SESSION['theme'] : 'dark';
     <script src="assets/js/script.js" defer></script>
 </head>
 <body class="<?= $theme === 'light' ? 'light' : 'dark' ?>">
-<h1>Dit is de <?= isset($title) ? strtolower($title) : 'home' ?> page</h1>
     <nav>
         <a href="./home">Home</a> |
         <a href="./projects">Projects</a> |
         <a href="./contact">Contact</a> |
     </nav>
-    <br>
-    <form class="themeForm" method="GET">
-        <input type="hidden" name="theme" class="themeInput">
-    </form>
-    <button onclick="toggleTheme()">Toggle theme</button>
+
+    <main>
+        <?php if(isset($title) && $title === '404'): ?>
+        <h1><?= strtolower($title) ?> page not found!</h1>
+        <a href="./home">Return to the home page</a>
+        <?php else: ?>
+        <h1>Dit is de <?= isset($title) ? strtolower($title) : 'home' ?> page</h1>
+        <?php endif; ?>
+        <br>
+
+        <?php if(isset($title) && $title !== '404'): ?>
+        <form class="themeForm" method="GET">
+            <input type="hidden" name="theme" class="themeInput">
+        </form>
+        <button onclick="toggleTheme()">Toggle theme</button>
+        <?php endif; ?>
+    </main>
+
 </body>
 </html>
