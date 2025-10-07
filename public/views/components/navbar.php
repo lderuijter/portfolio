4d3347cf3febd6e4e8548a8f6eb2bee94c5085b4
Lucas de Ruijter <lucas.deruijter@reyez.nl>
2025-10-07 09:39:37 +0200

    navbar styling start

--- public/views/components/navbar.php
+++ public/views/components/navbar.php
@@ -14,28 +14,31 @@ $theme = isset($_SESSION['theme']) ? $_SESSION['theme'] : 'dark';
     <script src="assets/js/script.js" defer></script>
 </head>
 <body class="<?= $theme === 'light' ? 'light' : 'dark' ?>">
-    <nav>
-        <a href="./home">Home</a> |
-        <a href="./projects">Projects</a> |
-        <a href="./contact">Contact</a> |
-    </nav>
+<div class="container">
+    <div class="navbar">
+        <nav>
+            <a href="./home">Home</a>
+            <a href="./projects">Projects</a>
+            <a href="./contact">Contact</a>
+        </nav>
+    </div>
 
     <main>
-        <?php if(isset($title) && $title === '404'): ?>
-        <h1><?= strtolower($title) ?> page not found!</h1>
-        <a href="./home">Return to the home page</a>
+        <?php if (isset($title) && $title === '404'): ?>
+            <h1><?= strtolower($title) ?> page not found!</h1>
+            <a href="./home">Return to the home page</a>
         <?php else: ?>
-        <h1>Dit is de <?= isset($title) ? strtolower($title) : 'home' ?> page</h1>
+            <h1>Dit is de <?= isset($title) ? strtolower($title) : 'home' ?> page</h1>
         <?php endif; ?>
         <br>
 
-        <?php if(isset($title) && $title !== '404'): ?>
-        <form class="themeForm" method="GET">
-            <input type="hidden" name="theme" class="themeInput">
-        </form>
-        <button onclick="toggleTheme()">Toggle theme</button>
+        <?php if (isset($title) && $title !== '404'): ?>
+            <form class="themeForm" method="GET">
+                <input type="hidden" name="theme" class="themeInput">
+            </form>
+            <button onclick="toggleTheme()">Toggle theme</button>
         <?php endif; ?>
     </main>
-
+</div>
 </body>
 </html>
