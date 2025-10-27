907019e390fc025280beab0ba4b8ca87a58e2e93
Lucas de Ruijter <lucas.deruijter@reyez.nl>
2025-10-27 15:51:36 +0100

    begonnen met home page styling (about me en foto)

--- public/views/home.php
+++ public/views/home.php
@@ -1,3 +1,21 @@
+<?php
+// de base variabele wordt altijd opgehaald uit de config.php
+/** @var $base */
+?>
+
+<h1 class="page-heading">Home page</h1>
+
 <div class="container">
-    <h1 class="page-heading">Home page</h1>
+    <div class="circle-image">
+        <img src="<?= $base ?>assets/images/lucas.png" alt="profile">
+    </div>
+    <div class="about-me-text">
+        <h2>About me</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+    </div>
+    <div class="about-me-icons">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+    </div>
 </div>
+
