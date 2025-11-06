95202b627b09c6b24eb098a697b2e83e4e33706c
Lucas de Ruijter <lucas.deruijter@reyez.nl>
2025-11-06 21:31:47 +0100

    CV update en toevoegen aan website

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -8,6 +8,7 @@
     <div class="nav-links">
         <a class="<?= (PAGE === 'home') ? 'active' : '' ?>" href="<?= BASE_URL ?>home">Home</a>
         <a class="<?= (PAGE === 'projects') ? 'active' : '' ?>" href="<?= BASE_URL ?>projects">Projects</a>
+        <a target="_blank" href="<?= BASE_URL ?>assets/resume/Lucas_CV_2025.pdf">CV</a>
         <a class="<?= (PAGE === 'contact') ? 'active' : '' ?>" href="<?= BASE_URL ?>contact">Contact</a>
 
         <!-- Als je bent ingelogd moet hier logout komen te staan -->
